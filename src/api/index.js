62e37e29ad5da98a3c125bff884737b74d66cbf2
Ameth Ordoñez <amethgabriel@hotmail.com>
2023-02-17 15:19:11 -0500

    Fix env variables

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,7 @@ export default async function apiOpenAI({ prompt }) {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer 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`
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`
       },
       body: JSON.stringify({
         model: 'text-davinci-003',
